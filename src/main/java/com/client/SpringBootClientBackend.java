a579a83c2ace2453aa2bc3eaa3013187305ba195
Martin <martin.alejandro.grellet@gmail.com>
2019-05-25 18:57:18 -0300

    Initial spring boot app with gradle and MySql connection settings

---
+++ src/main/java/com/client/SpringBootClientBackend.java
@@ -0,0 +1,13 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientBackend.class, args);
+	}
+
+}
